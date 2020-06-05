d830f9d2a93e6a6dd1d55bf2e51d43c9c9a41dbc
Divyatmika S <divyatmika@Divyatmikas-MacBook-Pro.local>
2020-06-04 20:34:42 -0700

    Updates

---
+++ precache-manifest.ad3cca9f0037ee78617c2b347b8ac12d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e5a9f611d63f6b2f6d04afb07ff29815",
+    "url": "/To-Do-List-ReactApp/index.html"
+  },
+  {
+    "revision": "0b3c801380900e56e166",
+    "url": "/To-Do-List-ReactApp/static/css/main.a4507ccd.chunk.css"
+  },
+  {
+    "revision": "c22ce226bc9b079ee3d5",
+    "url": "/To-Do-List-ReactApp/static/js/2.b0ebaed3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/To-Do-List-ReactApp/static/js/2.b0ebaed3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b3c801380900e56e166",
+    "url": "/To-Do-List-ReactApp/static/js/main.85d312b5.chunk.js"
+  },
+  {
+    "revision": "16e9643d94cdc4faf7df",
+    "url": "/To-Do-List-ReactApp/static/js/runtime-main.d3c7505e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/To-Do-List-ReactApp/static/media/logo.5d5d9eef.svg"
+  }
+]);
